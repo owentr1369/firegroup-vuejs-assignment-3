501d42217c22427777fdafc5581b059fc4f0e0bc
TranTam <tamvaa1306@gmail.com>
2022-07-09 17:08:11 +0700

    png

--- src/modules/stores/views/stores.vue
+++ src/modules/stores/views/stores.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="stores">
-    <h1>Stores</h1>
+    <div class="products">
+      <div class="products_head">
+        <h1 class="products_head-title">Stores</h1>
+        <button class="products_head-btn">
+          <img src="@/assets/svg/plus.svg" alt="" />
+          <span>Add new store</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
